48151fec096e6db637deae2f72b08f8a592e1788
Jahdi <jahidulkyau@gmail.com>
2025-06-14 15:41:11 +0600

    add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
+import 'dart:ui';
+
 import 'package:crarty_bay/app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  //framework error
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  //framework async error
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   runApp(CraftyBay());
 }
